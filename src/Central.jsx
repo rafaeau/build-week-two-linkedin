970f5a51104c7ba458ce69fe86920877bf660a55
TheJhonni <davideconi91@gmail.com>
2022-01-17 15:43:32 +0100

    Central part almost done

--- src/Central.jsx
+++ src/Central.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { BiPencil } from "react-icons/bi";
+import { AiOutlineDown, AiOutlinePlus } from "react-icons/ai";
 
 function Central() {
   return (
     <>
       {/* START CENTRAL COMPONENTS */}
 
-      <div className="row">
+      <div id="Central-Main-Component" className="row my-5 mx-auto">
         {/* FIrst div container with avatar img */}
         <div
           id="Central-firstDiv"
@@ -26,7 +27,7 @@ function Central() {
             />
             <BiPencil id="Central-pencil1" />
             <div id="Central-First" className="ml-4">
-              <div className="mx-2 mt- px-2 d-flex flex-column justify-content-start ">
+              <div className="mx-2 px-2 d-flex flex-column justify-content-start ">
                 <div className="d-flex flex-row justify-content-between">
                   <div className="d-flex flex-row mt-5">
                     <h3 className="h3">Name Username </h3>
@@ -50,9 +51,11 @@ function Central() {
                 id="Central-buttons-container"
                 className="d-flex justify-content-start align-items-center my-4"
               >
-                <button className="Central-btn-blue mx-2">Open to</button>
-                <button className="Central-btn-grey mx-2">Add section</button>
-                <button className="Central-btn-round mx-2">...</button>
+                <button className="Central-btn-blue mx-2 px-3">Open to</button>
+                <button className="Central-btn-grey mx-2 px-3">
+                  Add section
+                </button>
+                <button className="Central-btn-round mx-2 px-3">More</button>
               </div>
             </div>
           </div>
@@ -63,13 +66,13 @@ function Central() {
 
         <div
           id="Central-Second"
-          className="col-2 col-md-10 Central-container mt-4 mx-auto ml-3 my-4"
+          className="col-2 col-md-10 Central-container mt-4 mx-auto ml-3 py-4 "
         >
-          <div className="d-flex flex-row justify-content-between">
+          <div className="d-flex flex-row justify-content-between mx-2 px-2 ">
             <h4 className="text-semibold">About</h4>
             <BiPencil id="" className="Central-pencil2" />
           </div>
-          <div className="d-flex flex-column justify-content-start mt-4">
+          <div className="d-flex flex-column justify-content-start mt-4 mx-2 px-2 ">
             <p className="Central-p1 text-muted">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod,
               fugit quae quaerat sint corrupti adipisci ut iure cupiditate ipsam
@@ -85,8 +88,304 @@ function Central() {
         {/* End Second Central div container */}
 
         {/* Start Third Central div container */}
+        <div
+          id="Central-Second"
+          className="col-2 col-md-10 Central-container d-flex flex-column justify-content-start mt-4 mx-auto ml-3 py-4"
+        >
+          <div className="mx-2 px-2">
+            <h4 className="text-semibold">Activity</h4>
+            <a href="#">582 followers</a>
+          </div>
 
+          <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+            <img
+              className="Central-random-post-img cursor-pointer"
+              src="https://th.bing.com/th/id/OIP.kQIwQPBkDnBn84Ogf2WhpgHaG1?w=196&h=181&c=7&r=0&o=5&pid=1.7"
+              alt=""
+            />
+            <a className="Central-decoration-none" href="#">
+              <p className="Central-p1 font-bolder mx-2 mt-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Consequuntur dolorem, ipsam quibusdam accusantium obcaecati.
+                <p className="text-muted">Name Username commented</p>
+              </p>
+            </a>
+          </div>
+          <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+            <img
+              className="Central-random-post-img cursor-pointer"
+              src="https://th.bing.com/th/id/OIP.kQIwQPBkDnBn84Ogf2WhpgHaG1?w=196&h=181&c=7&r=0&o=5&pid=1.7"
+              alt=""
+            />
+            <a className="Central-decoration-none" href="#">
+              <p className="Central-p1 font-bolder mx-2 mt-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Consequuntur dolorem, ipsam quibusdam accusantium obcaecati.
+                <p className="text-muted">Name Username commented</p>
+              </p>
+            </a>
+          </div>
+          <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+            <img
+              className="Central-random-post-img cursor-pointer"
+              src="https://th.bing.com/th/id/OIP.kQIwQPBkDnBn84Ogf2WhpgHaG1?w=196&h=181&c=7&r=0&o=5&pid=1.7"
+              alt=""
+            />
+            <a className="Central-decoration-none" href="#">
+              <p className="Central-p1 font-bolder mx-2 mt-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Consequuntur dolorem, ipsam quibusdam accusantium obcaecati.
+                <p className="text-muted">Name Username commented</p>
+              </p>
+            </a>
+          </div>
+          <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+            <img
+              className="Central-random-post-img cursor-pointer"
+              src="https://th.bing.com/th/id/OIP.kQIwQPBkDnBn84Ogf2WhpgHaG1?w=196&h=181&c=7&r=0&o=5&pid=1.7"
+              alt=""
+            />
+            <a className="Central-decoration-none" href="#">
+              <p className="Central-p1 font-bolder mx-2 mt-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Consequuntur dolorem, ipsam quibusdam accusantium obcaecati.
+                <p className="text-muted">Name Username commented</p>
+              </p>
+            </a>
+          </div>
+          <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+            <img
+              className="Central-random-post-img cursor-pointer"
+              src="https://th.bing.com/th/id/OIP.kQIwQPBkDnBn84Ogf2WhpgHaG1?w=196&h=181&c=7&r=0&o=5&pid=1.7"
+              alt=""
+            />
+            <a className="Central-decoration-none" href="#">
+              <p className="Central-p1 font-bolder mx-2 mt-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Consequuntur dolorem, ipsam quibusdam accusantium obcaecati.
+                <p className="text-muted">Name Username commented</p>
+              </p>
+            </a>
+          </div>
+        </div>
         {/* End Third  Central div container */}
+
+        {/* Start Fourth Central div container */}
+        <div
+          id="Central-Second"
+          className="col-2 col-md-10 Central-container d-flex flex-column justify-content-start mt-4 mx-auto ml-3 py-4"
+        >
+          <div className="d-flex flex-row justify-content-between mx-2">
+            <h4 className="text-semibold">Experience</h4>
+            <AiOutlinePlus className="Central-Plus-Icon mb-4" />
+          </div>
+
+          <div className="d-flex justify-content-between">
+            <div>
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.0MG3R0maOfwdsab0MDOcywHaHa?w=201&h=200&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Some random Working experience.
+                      <p className="text-muted">
+                        Name Company where Name Username worked for
+                        <br /> Period from... to... when Name Username worked
+                        for
+                        <br /> Location Company where Name Username worked at
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <hr />
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.0MG3R0maOfwdsab0MDOcywHaHa?w=201&h=200&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Some random Working experience.
+                      <p className="text-muted">
+                        Name Company where Name Username worked for
+                        <br /> Period from... to... when Name Username worked
+                        for
+                        <br /> Location Company where Name Username worked at
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <hr />
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.0MG3R0maOfwdsab0MDOcywHaHa?w=201&h=200&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Some random Working experience.
+                      <p className="text-muted">
+                        Name Company where Name Username worked for
+                        <br /> Period from... to... when Name Username worked
+                        for
+                        <br /> Location Company where Name Username worked at
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <hr />
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.0MG3R0maOfwdsab0MDOcywHaHa?w=201&h=200&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Some random Working experience.
+                      <p className="text-muted">
+                        Name Company where Name Username worked for
+                        <br /> Period from... to... when Name Username worked
+                        for
+                        <br /> Location Company where Name Username worked at
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <hr />
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.0MG3R0maOfwdsab0MDOcywHaHa?w=201&h=200&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Some random Working experience.
+                      <p className="text-muted">
+                        Name Company where Name Username worked for
+                        <br /> Period from... to... when Name Username worked
+                        for
+                        <br /> Location Company where Name Username worked at
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <BiPencil id="" className="Central-pencil2" />
+          </div>
+
+          {/* Start Education Section */}
+          <hr />
+          <div className="d-flex flex-row justify-content-between mx-2 mt-3">
+            <h4 className="text-semibold">Education</h4>
+            <AiOutlinePlus className="Central-Plus-Icon mb-4" />
+          </div>
+
+          <div className="d-flex justify-content-between">
+            <div>
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.aYDYDAPxRyTQ9yRbV-ps3wAAAA?w=161&h=180&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Strive School
+                      <p className="text-muted">
+                        FrontEnd
+                        <br /> 2021
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <hr />
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.QPXu4Znw-lVZad4i9g2VwgHaG-?w=193&h=182&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Radford University
+                      <p className="text-muted">
+                        Languages and advances avances for advertisment advices
+                        <br /> Activities and Secret blood societies
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <hr />
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.aYDYDAPxRyTQ9yRbV-ps3wAAAA?w=161&h=180&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Strive School
+                      <p className="text-muted">
+                        FrontEnd
+                        <br /> 2021
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <hr />
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.QPXu4Znw-lVZad4i9g2VwgHaG-?w=193&h=182&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Radford University
+                      <p className="text-muted">
+                        Languages and advances avances for advertisment advices
+                        <br /> Activities and Secret blood societies
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <div className="d-flex my-3 align-items-center justify-content-start px-3 ml-2 Central-Shadow-Squared">
+                <p className="text-muted">Show 1 more education</p>
+                <AiOutlineDown className="mb-3 mx-2" />
+              </div>
+            </div>
+
+            <BiPencil id="" className="Central-pencil2" />
+          </div>
+          {/*End Education Section */}
+        </div>
+        {/* End Fourth Central div container */}
       </div>
 
       {/* END CENTRAL COMPONENT */}
