43fc4f8a4925de952ec458bd4f0099303e1549aa
Setimarz108 <66173614+Setimarz108@users.noreply.github.com>
2022-01-17 18:33:02 +0100

    Code Refactor
    Deleted Unused version of header.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-//import Header from './Components/Header'
-import Header2 from "./Components/Header2";
+import Header from "./Components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Central from "./Central";
@@ -9,8 +8,8 @@ import Central from "./Central";
 function App() {
   return (
     <>
-      {/* <Header/> */}
-      <Header2 />
+     
+      <Header />
       <Central />
     </>
   );
