4ae51a2e5317344b4a963caa6cd7ef8a3a02b4c8
Rafael Ferreira <rafaelnevesf@gmail.com>
2022-01-17 13:46:12 +0100

    LanguageBar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import LanguageBar from './components/LanguageBar';
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-8 mt-4'>
+
+        </div>
+        <div className='col-2 mt-4'>
+          <LanguageBar/>
+        </div>
+      </div>
+    </div>
   );
 }
 
