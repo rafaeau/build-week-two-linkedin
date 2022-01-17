629f85e53619236fe37234546fcf3950d92e6f8b
Setimarz108 <66173614+Setimarz108@users.noreply.github.com>
2022-01-17 15:44:11 +0100

    header Demo
    header Demo  / made with REact components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Header from './Components/Header'
+import Header2 from './Components/Header2'
 
 function App() {
   return (
     <>
-      <Header/>
+      {/* <Header/> */}
+      <Header2/>
     </>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,63 +3,80 @@ import {
   Navbar,
   FormControl,
   Nav,
-  NavDropdown,
   Form,
-  Button,
-  Container,
+  
 } from "react-bootstrap";
 import "./Header.css";
-import { BsGrid3X3GapFill } from "react-icons/bs";
-import { AiFillHome } from "react-icons/ai";
+import { BsGrid3X3GapFill, BsFillBriefcaseFill ,BsSearch} from "react-icons/bs";
+import { AiFillHome, AiFillMessage } from "react-icons/ai";
+import { IoNotificationsSharp } from "react-icons/io5";
+import { BiUserCircle } from "react-icons/bi";
+import {MdArrowDropDown } from "react-icons/md";
+
 
 export default function Header() {
   return (
+
+
     <Navbar className="mainDiv">
       <Navbar.Brand href="#home" id="logo">
         <img src="https://www.iconpacks.net/icons/2/free-linkedin-logo-icon-2430-thumb.png"></img>
       </Navbar.Brand>
       <Form inline>
+        
         <FormControl
           type="text"
           placeholder="Search"
-          className="mr-sm-2"
-          style={{ height: "34px" }}
+          className=" search mr-sm-2"
+          style={{ height: "34px",}}
         />
+        
       </Form>
+
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className=" ml-5 mr-auto d-flex justify-content-center">
-          <div className="work d-flex flex-column align-content-center">
-            <img src="https://raw.githubusercontent.com/CleverProgrammers/cp-linkedin-clone/f014d361d787029f15ea0f0f78c053d8c214f138/public/images/nav-home.svg" alt="" srcset="" />
+      
+        
+      <div className="px-4">
+        <Nav className=" innerDiv ml-5 mr-auto d-flex justify-content-center px-5">
+          <div className="work d-flex flex-column align-content-center pl-3">
+            <AiFillHome className="nav_icon align-self-center mt-3 "/>
             <Nav.Link href="#home">Home</Nav.Link>
           </div>
 
-          <div className="work d-flex flex-column align-content-center">
+          <div className="work d-flex flex-column align-content-center pl-3 ml-2">
+            <img  className="nav_icon align-self-center mt-3 " style={{ width: '24px', height:"24px"}} src="https://raw.githubusercontent.com/CleverProgrammers/cp-linkedin-clone/f014d361d787029f15ea0f0f78c053d8c214f138/public/images/nav-network.svg"></img>
+
             <Nav.Link href="#link">My Network</Nav.Link>
           </div>
-          <div className="work d-flex flex-column align-content-center">
+          <div className="work d-flex flex-column align-content-center pl-3 ml-2">
+            <BsFillBriefcaseFill className="nav_icon align-self-center mt-3"/>
             <Nav.Link href="#link">Jobs</Nav.Link>
           </div>
-          <div className="work d-flex flex-column align-content-center">
+
+          <div className="work d-flex flex-column align-content-center pl-3 ml-2">
+            <AiFillMessage className="nav_icon align-self-center mt-3"/>
             <Nav.Link href="#link">Messaging</Nav.Link>
           </div>
-          <div className="work d-flex flex-column align-content-center">
-            {" "}
+          <div className="work d-flex flex-column align-content-center pl-3 ml-2">
+            <IoNotificationsSharp className="nav_icon align-self-center mt-3"/>
             <Nav.Link href="#link">Notifications</Nav.Link>{" "}
           </div>
-          <div className="work d-flex flex-column align-content-center">
-            <Nav.Link href="#link">Me</Nav.Link>{" "}
+          <div className="work d-flex flex-column align-content-center pl-1">
+            <BiUserCircle className="nav_icon align-self-center mt-3 ml-3"></BiUserCircle>
+            <Nav.Link href="#link" className="ml-4">Me<MdArrowDropDown style={{position:"relative", marginLeft:"-5px",float: 'right', marginTop:"-12%", width:"30px", height:"30px" }}/></Nav.Link>
           </div>
-        </Nav>
-        <div className="right">
-          <div className="work d-flex flex-column align-content-center">
-            <BsGrid3X3GapFill className="align-self-center mt-3"></BsGrid3X3GapFill>
-            <Nav.Link href="#link" className="">
-              work
+        </Nav></div>
+
+        <div className="right ">
+          <Nav>
+          <div className="work d-flex flex-column  mt-1">
+            <BsGrid3X3GapFill className="nav_icon align-self-center mt-3" style={{marginLeft: "-30px"}}></BsGrid3X3GapFill>
+            <Nav.Link href="#link"  style={{marginLeft: "-20px"}} className="">
+              work <MdArrowDropDown style={{position:"relative", marginLeft:"-3px",float: 'right', marginTop:"-12%", width:"30px", height:"30px" }}/>
             </Nav.Link>
-          </div>
+          </div></Nav>
         </div>
-      </Navbar.Collapse>
+      
     </Navbar>
   );
 }
