d86a2bc25c276043d96192128c52339c889a08e9
TheJhonni <davideconi91@gmail.com>
2022-01-18 17:41:56 +0100

    commit after debrief

--- src/Components/Central2.jsx
+++ src/Components/Central2.jsx
@@ -1,12 +1,11 @@
 import "../App.css";
 import { BiPencil } from "react-icons/bi";
 import { BsBookmarkFill, BsFillPeopleFill } from "react-icons/bs";
-import { AiOutlineClose } from "react-icons/ai";
+import { AiOutlineClose, AiOutlineDown, AiOutlinePlus } from "react-icons/ai";
 import { GiFallingStar } from "react-icons/gi";
 import { FaSatelliteDish } from "react-icons/fa";
-import Experience from "./Experience";
 
-function Central(props) {
+function Central() {
   return (
     <>
       {/* START CENTRAL COMPONENTS */}
@@ -34,18 +33,19 @@ function Central(props) {
               <div className="mx-2 px-2 d-flex flex-column justify-content-start ">
                 <div className="d-flex flex-row justify-content-between">
                   <div className="d-flex flex-row mt-5">
-                    <h3 className="h3"> </h3>
+                    <h3 className="h3">Name Username</h3>
+                    <p className="Central-p1 text-muted ml-1 mt-1">(He/Him)</p>
                   </div>
                   <div className="d-flex justify-content-start align-items-center mx-2 px-1">
                     <img
                       className="Central-Strive-img cursor-pointer"
-                     
+                      src="https://th.bing.com/th/id/OIP.aYDYDAPxRyTQ9yRbV-ps3wAAAA?w=161&h=180&c=7&r=0&o=5&pid=1.7"
                       alt=""
                     />
                     <div className="mt-3 cursor-pointer">
                       <a className="Central-decoration-none" href="#">
                         <p className="Central-p1 font-bolder ml-2">
-                          
+                          Strive School
                         </p>
                       </a>
                     </div>
@@ -54,14 +54,14 @@ function Central(props) {
                   <BiPencil id="" className="Central-pencil2" />
                 </div>
 
-                <h6 className="text-semibold"></h6>
+                <h6 className="text-semibold">Random stuff about this user</h6>
 
                 <a className="Central-p1 my-2 text-muted" href="#">
                   Strive school
                 </a>
 
                 <p className="Central-p1 text-muted">
-                   - <a href="">Contact Info</a>
+                  Paris, France, Location at: .... - <a href="">Contact Info</a>
                 </p>
                 <a href="">500+ connections</a>
               </div>
@@ -291,7 +291,220 @@ function Central(props) {
         </div>
         {/* End 5th  Central div container */}
         {/* Start 6th Central div container */}
-       <Experience experiences={props.experiences} />
+        <div
+          id="Central-Second"
+          className="col-2 col-md-10 Central-container d-flex flex-column justify-content-start mt-4 mx-auto ml-3 py-4"
+        >
+          <div className="d-flex flex-row justify-content-between mx-2">
+            <h4 className="text-semibold">Experience</h4>
+            <AiOutlinePlus className="Central-Plus-Icon mb-4" />
+          </div>
+
+          <div className="d-flex justify-content-between">
+            <div>
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.0MG3R0maOfwdsab0MDOcywHaHa?w=201&h=200&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Some random Working experience.
+                      <p className="text-muted">
+                        Name Company where Name Username worked for
+                        <br /> Period from... to... when Name Username worked
+                        for
+                        <br /> Location Company where Name Username worked at
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <hr />
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.0MG3R0maOfwdsab0MDOcywHaHa?w=201&h=200&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Some random Working experience.
+                      <p className="text-muted">
+                        Name Company where Name Username worked for
+                        <br /> Period from... to... when Name Username worked
+                        for
+                        <br /> Location Company where Name Username worked at
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <hr />
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.0MG3R0maOfwdsab0MDOcywHaHa?w=201&h=200&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Some random Working experience.
+                      <p className="text-muted">
+                        Name Company where Name Username worked for
+                        <br /> Period from... to... when Name Username worked
+                        for
+                        <br /> Location Company where Name Username worked at
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <hr />
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.0MG3R0maOfwdsab0MDOcywHaHa?w=201&h=200&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Some random Working experience.
+                      <p className="text-muted">
+                        Name Company where Name Username worked for
+                        <br /> Period from... to... when Name Username worked
+                        for
+                        <br /> Location Company where Name Username worked at
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <hr />
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.0MG3R0maOfwdsab0MDOcywHaHa?w=201&h=200&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Some random Working experience.
+                      <p className="text-muted">
+                        Name Company where Name Username worked for
+                        <br /> Period from... to... when Name Username worked
+                        for
+                        <br /> Location Company where Name Username worked at
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <BiPencil id="" className="Central-pencil2" />
+          </div>
+
+          {/* Start Education Section */}
+          <hr />
+          <div className="d-flex flex-row justify-content-between mx-2 mt-3">
+            <h4 className="text-semibold">Education</h4>
+            <AiOutlinePlus className="Central-Plus-Icon mb-4" />
+          </div>
+
+          <div className="d-flex justify-content-between">
+            <div>
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.aYDYDAPxRyTQ9yRbV-ps3wAAAA?w=161&h=180&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Strive School
+                      <p className="text-muted">
+                        FrontEnd
+                        <br /> 2021
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <hr />
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.QPXu4Znw-lVZad4i9g2VwgHaG-?w=193&h=182&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Radford University
+                      <p className="text-muted">
+                        Languages and advances avances for advertisment advices
+                        <br /> Activities and Secret blood societies
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <hr />
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.aYDYDAPxRyTQ9yRbV-ps3wAAAA?w=161&h=180&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Strive School
+                      <p className="text-muted">
+                        FrontEnd
+                        <br /> 2021
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <hr />
+              <div className="d-flex justify-content-start align-items-center mx-2 px-1">
+                <img
+                  className="Central-random-post-img cursor-pointer"
+                  src="https://th.bing.com/th/id/OIP.QPXu4Znw-lVZad4i9g2VwgHaG-?w=193&h=182&c=7&r=0&o=5&pid=1.7"
+                  alt=""
+                />
+                <div className="mt-3 cursor-pointer">
+                  <a className="Central-decoration-none" href="#">
+                    <p className="Central-p1 font-bolder mx-4">
+                      Radford University
+                      <p className="text-muted">
+                        Languages and advances avances for advertisment advices
+                        <br /> Activities and Secret blood societies
+                      </p>
+                    </p>
+                  </a>
+                </div>
+              </div>
+              <div className="d-flex my-3 align-items-center justify-content-start ml-2 Central-Shadow-Squared">
+                <p className="text-muted">Show 1 more education</p>
+                <AiOutlineDown className="mb-3 ml-2" />
+              </div>
+            </div>
+
+            <BiPencil id="" className="Central-pencil2" />
+          </div>
+          {/*End Education Section */}
+        </div>
         {/* End 6th Central div container
          */}
       </div>
--- src/components/ProfilesMapping.jsx
+++ src/components/ProfilesMapping.jsx
@@ -1,22 +1,26 @@
-import { fetchProfiles } from '../apicalls'
-import { useEffect, useState } from "react"
-import SingleProfile from './SingleProfile'
-import { Link } from 'react-router-dom'
+import { fetchProfiles } from "../apicalls";
+import { useEffect, useState } from "react";
+import SingleProfile from "./SingleProfile";
+import { Link, useParams } from "react-router-dom";
 
-const ProfilesMapping = ({btn}) => {
+const ProfilesMapping = ({ btn }) => {
+  const [profiles, setProfiles] = useState();
+  const params = useParams();
 
-    const [profiles, setProfiles] = useState()
-    useEffect(() => {
-        fetchProfiles().then((res) => setProfiles(res))
-    }, [])
+  useEffect(() => {
+    fetchProfiles().then((res) => setProfiles(res));
+  }, [params]);
 
-    return (
-        <>
-            {profiles && profiles.slice(295, 300).map((profile) => (
-                <Link to={'profile/' + profile._id + '/experiences'}><SingleProfile key={profile._id} btn={btn} profile={profile} /></Link>
-            ))}
-        </>
-    )
-}
+  return (
+    <>
+      {profiles &&
+        profiles.slice(295, 300).map((profile) => (
+          <Link to={"/profile/" + profile._id + "/experiences"}>
+            <SingleProfile key={profile._id} btn={btn} profile={profile} />
+          </Link>
+        ))}
+    </>
+  );
+};
 
-export default ProfilesMapping
+export default ProfilesMapping;
