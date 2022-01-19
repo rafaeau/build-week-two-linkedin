e2cfa72474d54e9b9b80c810c90aa8ea54db8d53
Rafael Ferreira <rafaelnevesf@gmail.com>
2022-01-19 10:30:16 +0100

    Homepage component

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -27,3 +27,13 @@ export const fetchUser = async (userId) => {
     let user = await response.json()
     return user
 }
+
+export const fetchPosts = async () => {
+    let response = await fetch('https://striveschool-api.herokuapp.com/api/posts/', {
+        headers: {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMjFmMTRiYjUzZDAwMTViMTllZDciLCJpYXQiOjE2NDI0NDE3ODksImV4cCI6MTY0MzY1MTM4OX0.c8a_yy-ROyiriWmK5LnQYY8Gmrz8smjTnxvGxtDu-24"
+        }
+    })
+    let posts = await response.json()
+    return posts
+}
---
+++ src/views/Homepage.jsx
@@ -0,0 +1,22 @@
+import Ad from '../Components/Ad';
+import adImg from '../assets/linkedin-ad.jpg'
+
+export default function Homepage() {
+    return (
+        <>
+            <div className='container'>
+                <div className='row'>
+                    <div className="col-2">
+                        
+                    </div>
+                    <div className="col-7">
+                        
+                    </div>
+                    <div className="col-3">
+                        <Ad />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
