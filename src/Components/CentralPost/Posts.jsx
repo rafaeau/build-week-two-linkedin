cf41d82ac5be46244106b02aeaa0cf4b80ab8566
TheJhonni <davideconi91@gmail.com>
2022-01-19 12:14:57 +0100

    centrap Post section

---
+++ src/Components/CentralPost/Posts.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./CentralPost.css";
+import { CgProfile } from "react-icons/cg";
+import InputOptions from "./InputOptions";
+import { GoThumbsup } from "react-icons/go";
+import { FaRegCommentDots } from "react-icons/fa";
+import { RiShareForwardLine, RiSendPlaneLine } from "react-icons/ri";
+
+function Posts({ name, description, message, photoUrl }) {
+  return (
+    <div className="post">
+      <div className="post-header">
+        <CgProfile />
+        <div className="post-info">
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+
+      <div className="post-body">
+        <p>{message}</p>
+      </div>
+
+      <div className="post-buttons">
+        <InputOptions Icon={GoThumbsup} title="Like" color="gray" />
+        <InputOptions Icon={FaRegCommentDots} title="Comment" color="gray" />
+        <InputOptions Icon={RiShareForwardLine} title="Share" color="gray" />
+        <InputOptions Icon={RiSendPlaneLine} title="Send" color="gray" />
+      </div>
+    </div>
+  );
+}
+
+export default Posts;
