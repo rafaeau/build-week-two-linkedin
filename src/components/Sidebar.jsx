fa843be71403a9c2eb5430b56d30e23fc324087b
TheJhonni <davideconi91@gmail.com>
2022-01-21 15:45:58 +0100

    col d-none on sm

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,20 @@
-import Ad from './Ad';
-import CoursesSection from './CoursesSection';
-import LanguageBar from './LanguageBar';
-import PeopleAlsoViewed from './PeopleAlsoViewed';
-import PeopleYouMayKnow from './PeopleYouMayKnow';
+import Ad from "./Ad";
+import CoursesSection from "./CoursesSection";
+import LanguageBar from "./LanguageBar";
+import PeopleAlsoViewed from "./PeopleAlsoViewed";
+import PeopleYouMayKnow from "./PeopleYouMayKnow";
 
 const Sidebar = () => {
-    return (
-        <>
-            <LanguageBar />
-            <Ad />
-            <PeopleAlsoViewed btn={'Message'} />
-            <PeopleYouMayKnow btn={'Connect'} />
-            <CoursesSection />
-            <Ad />
-        </>
-    )
-}
+  return (
+    <div className="d-none d-md-block col-md-2">
+      <LanguageBar />
+      <Ad />
+      <PeopleAlsoViewed btn={"Message"} />
+      <PeopleYouMayKnow btn={"Connect"} />
+      <CoursesSection />
+      <Ad />
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
